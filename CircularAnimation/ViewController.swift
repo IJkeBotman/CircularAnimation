9bf268b17d43e7826aa1108cf6e816b4398164f9
IJke <ijke@overscope.net>
2017-12-10 12:38:32 +0100

    Create tappable circular animation.

--- CircularAnimation/ViewController.swift
+++ CircularAnimation/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Create new shapeLayer
+    let shapeLayer = CAShapeLayer()
+    var circularPath = UIBezierPath()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        setupTrackLayer()
+        setupShapeLayer()
+    
+        //Add shapeLayer to view
+        view.layer.addSublayer(shapeLayer)
+        
+        //Add gesture recognizer with action handleTap
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc func handleTap() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+    }
+    
+    func setupTrackLayer() {
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(trackLayer)
+    }
+    
+    func setupShapeLayer() {
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = 0
     }
 
     override func didReceiveMemoryWarning() {
