6fdf43aace9437e8eb828f8b8c6c64abf66b6132
IJke <ijke@overscope.net>
2018-05-20 13:38:47 +0200

    Clean up

--- CircularAnimation/ViewController.swift
+++ CircularAnimation/ViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDownloadDelegate {
 
+    let urlString = "https://upload.wikimedia.org/wikipedia/commons/e/e6/Clocktower_Panorama_20080622_20mb.jpg"
     var shapeLayer: CAShapeLayer!
     var pulsatingLayer: CAShapeLayer!
     var circularPath = UIBezierPath()
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
+    
     let percentageLabel: UILabel = {
         let label = UILabel()
         label.text = "Start"
@@ -25,15 +24,12 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         return label
     }()
     
-    private func setupNotificationObserver() {
-        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         setupNotificationObserver()
         
@@ -46,15 +42,23 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
         //Add gesture recognizer with action handleTap
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        
     }
     
+    @objc func handleTap() {
+        beginDownloadingFile()
+    }
+    
+    //Make sure the pulsating layer starts up again when the app returns to foreground.
     @objc private func handleEnterForeground() {
         animatePulsatingLayer()
     }
     
-    let urlString = "https://upload.wikimedia.org/wikipedia/commons/e/e6/Clocktower_Panorama_20080622_20mb.jpg"
+    //Add observer for when the app enters foreground.
+    private func setupNotificationObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
+    }
     
+    //Download file with URLSessionConfiguration to monitor the download progress.
     private func beginDownloadingFile() {
         print("Attempting to download file.")
         
@@ -69,6 +73,7 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
     }
     
+    //Delegate function that monitors bytes written and expected bytes to write. Set the percentage label and strokeEnd of the shapeLayer to the percentage downloaded.
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         
         let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
@@ -79,10 +84,12 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         }
     }
     
+    //Required protocol function
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         print("Finished downloading file.")
     }
     
+    //Animate the circle with CABasicAnimation, set fillMode and make sure the stroke is not removed after completion.
     fileprivate func animateCircle() {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
@@ -93,30 +100,14 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         shapeLayer.add(basicAnimation, forKey: "basicAnimation")
     }
     
+    //Add the percentageLabel to the center of the screen
     fileprivate func setupPercentageLayer() {
         view.addSubview(percentageLabel)
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         percentageLabel.center = view.center
     }
     
-    @objc func handleTap() {
-        beginDownloadingFile()
-        
-    }
-    
-    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
-        let layer = CAShapeLayer()
-        circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        layer.path = circularPath.cgPath
-        layer.strokeColor = strokeColor.cgColor
-        layer.fillColor = fillColor.cgColor
-        layer.lineWidth = 20
-        layer.lineCap = kCALineCapRound
-        layer.position = view.center
-        
-        return layer
-    }
-    
+    //Shape that is underneath the animating stroke. Shape that is pulsating out from the track layer.
     func setupTrackLayer() {
         
         pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: .pulsatingFillColor)
@@ -133,10 +124,25 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
     }
     
+    //Setup and rotate shapeLayer to make the stroke start at the top of the shape.
     func setupShapeLayer() {
         shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
         shapeLayer.strokeEnd = 0
         shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+    }
+    
+    //Setup circle shape with bezier path, strokeColor, fillColor, lineWidth, lineCap, and position.
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.fillColor = fillColor.cgColor
+        layer.lineWidth = 20
+        layer.lineCap = kCALineCapRound
+        layer.position = view.center
+        
+        return layer
     }
     
     private func animatePulsatingLayer() {
